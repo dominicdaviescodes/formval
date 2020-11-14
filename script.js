a46bc35b8bab1e056c50b25421b9c0a2cd35941c
dominicdaviescodes <codewithdom@gmail.com>
2020-11-14 14:10:38 +0000

    add form validations for empty fields

--- script.js
+++ script.js
@@ -1,2 +1,54 @@
 // A simple form validation project.  All fields are required.
 // id for javascript, class for styling.
+
+// 1. Grab the DOM elements
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Functions
+
+// Show input error message
+function showError(input, message) {
+  // need the parent
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show Success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+// Event Listeners
+
+// submit functionality - listen for submit, run function
+form.addEventListener('submit', function (e) {
+  // stop submitting
+  e.preventDefault();
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === '') {
+    showError(password2, 'Please confirm Password');
+  } else {
+    showSuccess(password2);
+  }
+});
